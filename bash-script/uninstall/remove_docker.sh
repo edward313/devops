1ef96b6faeaf4610f97b2282ba03f35070f35025
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2024-05-22 13:11:47 +0700

    add ci cd template

---
+++ bash-script/uninstall/remove_docker.sh
@@ -0,0 +1,3 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get autoremove --purge docker-ce docker-ce-cli containerd.io docker-compose-plugin
