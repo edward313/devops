b94d1e16a17774dfa3d6ab64db2e51f7932db307
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2024-05-30 10:35:58 +0700

    add bash for install nginx-proxy-manage

--- bash-script/setup/nginx-proxy-manage/nginx-proxy-manage.sh
+++ bash-script/setup/nginx-proxy-manage/nginx-proxy-manage.sh
@@ -1,4 +1,4 @@
 
 
 
-curl -o docker-compose.yml https://raw.githubusercontent.com/kestra-io/kestra/develop/docker-compose.yml
+curl -o docker-compose.yml https://raw.githubusercontent.com/edward313/devops-boilerplate/main/bash-script/setup/nginx-proxy-manage/docker-compose.yml
